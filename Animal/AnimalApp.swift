5eccc3d68d32023af62991975ddec9dca393ec12
Swathi <pinky.nani11@gmail.com>
2023-11-11 22:10:16 +0100

    Initial Commit

---
+++ Animal/AnimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalApp.swift
+//  Animal
+//
+//  Created by admin on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
